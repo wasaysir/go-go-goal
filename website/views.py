24e0eccdfa08ce6a55ff8f08420e283746b174f6
wasaysir <mwasaysaeed@gmail.com>
2022-01-09 03:33:27 -0500

    Functional User Setup

--- website/views.py
+++ website/views.py
@@ -6,6 +6,11 @@ from .form import SearchForm
 
 views = Blueprint('views', __name__)
 
+@views.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
